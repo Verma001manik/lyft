7ee1dcea6f7d46348fff528d5027dfdfc0d75785
Manik Verma <verma001manik@gmail.com>
2023-10-09 16:02:03 +0530

    done few refactoring

---
+++ battery/SpindlerBattery.py
@@ -0,0 +1,9 @@
+from car import Car 
+
+class  SpindlerBattery(Car):
+    def __init__(self,last_service_date,current_date):
+        self.last_service_date = last_service_date
+        self.current_date = current_date
+    
+    def needs_service(self):
+        pass 
--- car.py
+++ car.py
@@ -1,9 +1,12 @@
 from abc import ABC, abstractmethod
-
+# from engine.capulet_engine import CapuletEngine
+# from 
 
 class Car(ABC):
-    def __init__(self, last_service_date):
-        self.last_service_date = last_service_date
+    def __init__(self, engine, battery):
+        self.engine = engine 
+        self.battery = battery 
+
 
     @abstractmethod
     def needs_service(self):
